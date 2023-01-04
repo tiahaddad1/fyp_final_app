514f62f0a0ea618ddb7bdfa9600f32d37d98e43e
tiahaddad1 <haddad_tia@hotmail.com>
2023-01-04 17:07:21 +0000

    creating firebase account

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
 import 'View/userRole.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
